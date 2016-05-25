423f982d86a03bcb5ed224150a42f6b764094847
zsrichock <saza_thi@hotmail.com>
2016-05-25 16:54:41 +0700

    open project

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "timestamp", "updated"]
+    list_display_links = ["updated"]
+    list_filter = ["updated", "timestamp"]
+    list_editable = ["title"]
+    search_fields = ["title", "content"]
+   # inlines = [ "title" ]
+
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostModelAdmin)
