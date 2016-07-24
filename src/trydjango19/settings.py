2ad5da58d2a313b1b4d2994252fe51cea6372195
saza <saza_thi@hotmail.com>
2016-07-25 00:27:49 +0700

    27 of 38

--- src/trydjango19/settings.py
+++ src/trydjango19/settings.py
@@ -129,4 +129,5 @@ STATICFILES_DIRS = [
 
 #system  static file
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
